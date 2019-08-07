a4c937264705e858707769b45c75d4f8af0a6630
MerylGeugies <mgeugies@gmail.com>
2019-08-07 14:47:59 +0200

    Made model.js

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 function onListen() {
-  console.log(`Listining on :${port}`)
+  console.log(`Listening on :${port}`)
 }
 app.listen(port, onListen);
